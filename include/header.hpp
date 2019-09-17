35586a88b51a5c1a91919d2ec3860c046b57d49f
alexsun8 <alexsun24@yahoo.com>
2019-09-17 23:20:24 +0300

    t1

--- include/header.hpp
+++ include/header.hpp
@@ -10,77 +10,71 @@
 #include <sstream>
 
 class Experiment {
-private:
-    int number;
-    double buffer_size_kb;
-    double duration_ns;
-public:
-    Experiment(int num, double B_S_kb, double D_ns) {
-        number = num;
-        buffer_size_kb = B_S_kb;
-        duration_ns = D_ns;
-    }
-    ~Experiment() = default;
+ private:
+  int number;
+  double buffer_size_kb;
+  double duration_ns;
 
-    int num() {
-        return number;
-    }
+ public:
+  Experiment(int num, double B_S_kb, double D_ns) {
+    number = num;
+    buffer_size_kb = B_S_kb;
+    duration_ns = D_ns;
+  }
+  ~Experiment() = default;
 
-    std::string memory() {
-        std::string memory = "";
-        double buffer_size_mb = buffer_size_kb / 1024;
-        std::ostringstream ost;
-        if (buffer_size_kb > 1024) {
-            ost << buffer_size_mb;
-            memory += ost.str();
-            memory += " mb";
-        }
-        else {
-            ost << buffer_size_kb;
-            memory += ost.str();
-            memory += " kb";
-        }
-        return memory;
-    }
+  int num() { return number; }
 
-    double time() {
-        return duration_ns;
+  std::string memory() {
+    std::string memory = "";
+    double buffer_size_mb = buffer_size_kb / 1024;
+    std::ostringstream ost;
+    if (buffer_size_kb > 1024) {
+      ost << buffer_size_mb;
+      memory += ost.str();
+      memory += " mb";
+    } else {
+      ost << buffer_size_kb;
+      memory += ost.str();
+      memory += " kb";
     }
+    return memory;
+  }
+
+  double time() { return duration_ns; }
 };
 
 class Report {
-private:
-    std::string travel_variant = "";
-    std::vector<Experiment> experiments;
-public:
-    Report() = default;
-    Report(std::string T_V, std::vector<Experiment> ex_mass) {
-        travel_variant = T_V;
-        experiments = ex_mass;
-    }
+ private:
+  std::string travel_variant = "";
+  std::vector<Experiment> experiments;
 
-    ~Report() = default;
+ public:
+  Report() = default;
+  Report(std::string T_V, std::vector<Experiment> ex_mass) {
+    travel_variant = T_V;
+    experiments = ex_mass;
+  }
 
-    std::string tr_var() {
-        return travel_variant;
-    }
+  ~Report() = default;
 
-    std::vector<Experiment> exps() {
-        return experiments;
-    }
+  std::string tr_var() { return travel_variant; }
 
-    void clear() {
-        experiments.clear();
-        travel_variant.clear();
-    }
+  std::vector<Experiment> exps() { return experiments; }
 
-    void make(std::string T_V, std::vector<Experiment> ex_mass) {
-        travel_variant = T_V;
-        experiments = ex_mass;
-    }
+  void clear() {
+    experiments.clear();
+    travel_variant.clear();
+  }
+
+  void make(std::string T_V, std::vector<Experiment> ex_mass) {
+    travel_variant = T_V;
+    experiments = ex_mass;
+  }
 };
 
-Experiment experiment(int his_nuber, size_t n, size_t lenta, std::string& order);
+Experiment experiment(int his_nuber, size_t n, size_t lenta,
+                      std::string& order);
 
 int* make_buffer(size_t n, int lenta, std::string order);
 
@@ -94,5 +88,4 @@ void write_report(Report report);
 
 int mainF();
 
-
-#endif // INCLUDE_HEADER_HPP_
+#endif  // INCLUDE_HEADER_HPP_
